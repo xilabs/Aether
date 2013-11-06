ae03673808ac5607fa27e83740664da0a7293dfa
icecoldelf <lukedotson@gmail.com>
2013-11-06 18:35:59 -0500

    Integrating with the engine
    Integrated with the Aether engine using websockets to get all moxel
    objects and positions.

--- webclient/aetherObjects.js
+++ webclient/aetherObjects.js
@@ -3,6 +3,7 @@
 function Universe() {
     
     this.moxels = new Array();
+    this.objectTypes = new Array();
     
     this.createMoxel = function(position, radius) {
         
@@ -12,18 +13,26 @@ function Universe() {
         this.moxels.push(moxel);
         return moxel;
         
-    }
+    };
     
     this.updateUniverse = function() {
         
-    }
+    };
      
 }
 
 function Moxel() {
     
+    this.object_type_id = 1000;
     this.position;
     this.radius;
     this.happy = "hmm";
     
+    this.to_string = function(){
+
+		return "Moxel("+this.position[0]+","+this.position[1]+","+this.position[2]+")";
+    };
+    
+    
 }
+
--- webclient/render.js
+++ webclient/render.js
@@ -3,23 +3,35 @@ function Portal(universe) {
     this.u = universe;
     this.v = new View();
     this.scene = new THREE.Scene();
+    this.scene.fog = new THREE.FogExp2( 0xcccccc, 0.005);
     this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setClearColor( this.scene.fog.color, 1 );
     this.i = 0;
     this.canvas = document.body.appendChild( this.renderer.domElement );
     this.canvas.id = "portal";
     this.camera.position.z = 50;
+    this.happy;
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(1, 1, 1).normalize();
+    this.scene.add(directionalLight);
     
-    this.populateView = function() {
+    var ambientLight = new THREE.AmbientLight(0x222222);
+
+    ambientLight.position.set(-50,-50,-50).normalize();
+    this.scene.add(ambientLight);
+    
+    
+    this.populateView = function(n) {
         var moxels = this.u.moxels;
        
-        var i = 0;
+        var i = n;
         while(moxels[i]) {
             var moxel = moxels[i];
             
             var geometry = new THREE.SphereGeometry(moxel.radius,10,10); 
-            var material = new THREE.MeshNormalMaterial( {} ); 
+            var material = new THREE.MeshLambertMaterial({color: 'blue'}); 
             var sphere = new THREE.Mesh( geometry, material );
             sphere.position.x = moxel.position[0];
             sphere.position.y = moxel.position[1];
@@ -33,8 +45,15 @@ function Portal(universe) {
     };
     
     this.updateView = function() {
-        this.v.objects[0].position.x = this.u.moxels[0].position[0];
-        this.v.objects[0].position.y = this.u.moxels[0].position[1];
+        
+        var n = 0;
+        while (this.v.objects[n]) {
+            this.v.objects[n].position.x = this.u.moxels[n].position[0];
+            this.v.objects[n].position.y = this.u.moxels[n].position[1];
+            this.v.objects[n].position.z = this.u.moxels[n].position[2];
+            n++;
+        }
+       
     };
        
 }
