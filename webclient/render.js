7cd871cbf76abc73ae21cf938095ee0a07ab44fc
icecoldelf <lukedotson@gmail.com>
2013-11-07 23:28:21 -0500

    Clickable Moxels and command line
    Added command line and clickable moxels.... still need to clean up my
    code.. :(

--- webclient/render.js
+++ webclient/render.js
@@ -13,6 +13,21 @@ function Portal(universe) {
     this.canvas.id = "portal";
     this.camera.position.z = 50;
     this.happy;
+    this.projector = new THREE.Projector();
+    this.PI2 = Math.PI * 2;
+    this.particleMaterial = new THREE.SpriteCanvasMaterial( {
+
+        color: 0x000000,
+        program: function ( context ) {
+
+	context.beginPath();
+	context.arc( 0, 0, 1, 0, this.PI2, true );
+	context.fill();
+
+	}
+
+    });
+    
     var directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(1, 1, 1).normalize();
     this.scene.add(directionalLight);
@@ -55,9 +70,38 @@ function Portal(universe) {
         }
        
     };
+    
+    this.onPortalClick = function(event) {
+        
+        var offset = $("#portal").offset();
+        var relX = event.clientX - offset.left;
+        var relY = event.clientY - offset.top;
+        var pWidth = $("#portal").width();
+        var pHeight = $("#portal").height();
+        
+        event.preventDefault();
+        
+        var vector = new THREE.Vector3( ( relX / pWidth ) * 2 - 1, - ( relY / pHeight ) * 2 + 1, 0.5 );
+        
+        this.projector.unprojectVector( vector, this.camera );
+
+        var raycaster = new THREE.Raycaster( this.camera.position, vector.sub( this.camera.position ).normalize() );
+        var intersects = raycaster.intersectObjects( this.v.objects );
+
+        if ( intersects.length > 0 ) {
+
+            intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+
+        }
+    };
        
 }
 
 function View() {
     this.objects = new Array();
 }
+
+function CommandInterface() {
+    
+}
+
